798889d90f3b033d4b75e0fc35df96ee5aeb8b8d
Chaitra Mathur <chaitra_mathur@yahoo.com>
2015-04-22 17:41:52 -0400

    Testinggit add HypnoNerd/BNRReminderViewController.m

--- HypnoNerd/BNRReminderViewController.m
+++ HypnoNerd/BNRReminderViewController.m
@@ -31,7 +31,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view.
-  NSLog(@"BNRReminderViewController loaded.");
+  NSLog(@"BNRReminderViewController loaded.....");
 }
 
 - (void)didReceiveMemoryWarning {
